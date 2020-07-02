af6054ee829f79ce0636bba9d379e0d9ac338a70
peterwilmshurst <peterwilmshurst@hotmail.com>
2020-07-02 22:05:18 +0100

    adding layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
-//=include partials/fatNav.js
+//=include ../../node_modules/slick-carousel/slick/slick.js
 
+//=include partials/fatNav.js
 //=include partials/jquery.easing.1.3.js
 //=include partials/jquery.hoverflow.js
 //=include partials/face.js
+
+//=include partials/slider.js
 
 (function () {
     $.fatNav();
---
+++ src/js/partials/slider.js
@@ -0,0 +1,11 @@
+$('.kind-words-slider').slick({
+        dots: true,
+        arrows:false,
+        infinite: true,
+        autoplay: true,
+	    autoplaySpeed: 3000,
+        fade: true,
+        fadeSpeed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
